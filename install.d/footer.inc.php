30e5e9ad9e7f39363cecb8ba39289d5a4430a82c
x99bachelart <x99bachelart>
2003-09-02 21:55:25 +0000

    HTML default header and footer

---
+++ install.d/footer.inc.php
@@ -0,0 +1,16 @@
+<?php
+/********************************************************************************
+* install.d/footer.inc.php : HTML default footer
+* --------------------------
+*
+* This file is part of the banana distribution
+* Copyright: See COPYING files that comes with this distribution
+********************************************************************************/
+?>
+    </div>
+    <p class="footer">
+      <i>Banana</i>, a Web interface for a NNTP Server<br />
+      Developed under GPL License for <a href="http://www.polytechnique.org">Polytechnique.org</a>
+    </p>
+  </body>
+</html>
---
+++ install.d/header.inc.php
@@ -0,0 +1,23 @@
+<?php
+/********************************************************************************
+* install.d/header.inc.php : HTML default header
+* --------------------------
+*
+* This file is part of the banana distribution
+* Copyright: See COPYING files that comes with this distribution
+********************************************************************************/
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; 
+    charset=iso-8859-1">
+    <meta name="description" content="WebForum2/Banana">
+    <link href="css/style.css" type="text/css" rel="stylesheet"
+    media="screen">
+    <title>
+      Banana, a NNTP<->Web Gateway 
+    </title>
+  </head>
+  <body>
+    <div class="bloc">
