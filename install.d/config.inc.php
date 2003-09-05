9cf5b337baa2af3dec07308427a9f4872bde8ed8
x99bachelart <x99bachelart>
2003-09-05 12:31:21 +0000

    configuration files

---
+++ install.d/config.inc.php
@@ -0,0 +1,49 @@
+<?php
+
+/** comparison function for the overview 
+ * @param $a OBJECT spoolhead 
+ * @param $b OBJECT spoolhead
+ * @return
+ */
+
+function spoolcompare($a,$b) {
+  global $news;
+  return ($b->date>=$a->date);
+}
+
+// spool config in spool.inc.php
+$news['maxspool'] = 3000;
+
+// encoded headers
+$news['hdecode'] = array('from','name','organization','subject');
+
+// headers in article.php
+$news['head'] = array(
+  'From' => 'from',
+  'Subject' => 'subject',
+  'Newsgroups' => 'newsgroups',
+  'Followup-To' => 'followup',
+  'Date' => 'date',
+  'Organization' => 'organization',
+  'References' => 'references',
+  'X-Face' => 'xface',
+  );
+
+// overview configuration in article.php
+$news['threadtop'] = 5;
+$news['threadbottom'] = 5;
+
+// wordwrap configuration
+$news['wrap'] = 80;
+
+// overview configuration in thread.php
+$news['max'] = 50;
+
+// custom headers in post.php
+$news['customhdr'] = "Content-Type: text/plain; charset=iso-8859-15\n"
+  ."Mime-Version: 1.0\n"
+  ."Content-Transfer-Encoding: 8bit\n"
+  ."HTTP-Posting-Host: ".$_SERVER['REMOTE_HOST']."\n"
+  ."User-Agent: Banana 0.7beta\n";
+
+?>
---
+++ install.d/password.inc.php
@@ -0,0 +1,5 @@
+<?php
+$news['server']="localhost:119";
+$news["user"]=$_SESSION['login'];
+$news["pass"]=$_SESSION['passwd'];
+?>
