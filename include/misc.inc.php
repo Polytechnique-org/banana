f78f34d24e8c50d41eec95a5e46ca0a9f449af38
x2000habouzit <x2000habouzit>
2005-01-03 11:30:54 +0000

    exit format.inc.php

--- include/misc.inc.php
+++ include/misc.inc.php
@@ -137,6 +137,56 @@ function formatFrom($text) {
     return preg_replace("/\\\(\(|\))/","\\1",$result);
 }
 
+function displayshortcuts($first = -1) {
+    global $banana, $css;
+    $sname = basename($_SERVER['SCRIPT_NAME']);
+
+    echo "<div class=\"{$css['bananashortcuts']}\">";
+
+    if (function_exists('hook_displayshortcuts')) {
+        hook_displayshortcuts($sname, $first);
+    } else {
+        echo '[<a href="disconnect.php">'._b_('Déconnexion').'</a>] ';
+    }
+
+    switch ($sname) {
+        case 'thread.php' :
+            echo '[<a href="index.php">'._b_('Liste des forums').'</a>] ';
+            echo "[<a href=\"post.php?group={$banana->spool->group}\">"._b_('Nouveau message')."</a>] ";
+            if (sizeof($banana->spool->overview)>$banana->tmax) {
+                for ($ndx=1; $ndx<=sizeof($banana->spool->overview); $ndx += $banana->tmax) {
+                    if ($first==$ndx) {
+                        echo "[$ndx-".min($ndx+$banana->tmax-1,sizeof($banana->spool->overview))."] ";
+                    } else {
+                        echo "[<a href=\"?group={$banana->spool->group}&amp;first="
+                            ."$ndx\">$ndx-".min($ndx+$banana->tmax-1,sizeof($banana->spool->overview))
+                            ."</a>] ";
+                    }
+                }
+            }
+            break;
+        case 'article.php' :
+            echo '[<a href="index.php">'._b_('Liste des forums').'</a>] ';
+            echo "[<a href=\"thread.php?group={$banana->spool->group}\">{$banana->spool->group}</a>] ";
+            echo "[<a href=\"post.php?group={$banana->spool->group}&amp;id={$banana->post->id}&amp;type=followup\">"
+                ._b_('Répondre')."</a>] ";
+            if ($banana->post->checkcancel()) {
+                echo "[<a href=\"article.php?group={$banana->spool->group}&amp;id={$banana->post->id}&amp;type=cancel\">"
+                    ._b_('Annuler ce message')."</a>] ";
+            }
+            break;
+        case 'post.php' :
+            echo '[<a href="index.php">'._b_('Liste des forums').'</a>] ';
+            echo "[<a href=\"thread.php?group={$banana->spool->group}\">{$banana->spool->group}</a>]";
+            break;
+    }
+    echo '</div>';
+}
+
+/********************************************************************************
+ *  FORMATTING STUFF : BODY
+ */
+
 function wrap($text, $_prefix="")
 {
     $parts = preg_split("/\n-- ?\n/", $text);
