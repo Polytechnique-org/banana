ad7b9c56adeb270f613f2bec1c0f0de26e852e46
Pierre Habouzit (MadCoder <pierre.habouzit@m4x.org>
2005-02-07 00:08:19 +0000

    damn I hate euro symbols
    git-archimport-id: opensource@polytechnique.org--2005/banana--mainline--1.0--patch-17

--- banana/post.inc.php
+++ banana/post.inc.php
@@ -44,7 +44,7 @@ class BananaPost
         }
 
         if (preg_match('!charset=([^;]*)\s*(;|$)!', $this->headers['content-type'], $matches)) {
-            $this->body = iconv($matches[1], 'iso-8859-1', $this->body);
+            $this->body = iconv($matches[1], 'iso-8859-15', $this->body);
         }
     }
 
