7a0e2710704d196dc470cdc9d6a4b130fbf3b2e0
x2003bruneau <x2003bruneau@9869982d-c50d-0410-be91-f2a2ec7c7c7b>
2006-02-27 12:39:29 +0000

    Ajout de : * support succint des messages multipart (pas pour l'instant de distinction entre les types de multipart) * support des piÃšces jointes
    git-svn-id: svn+ssh://murphy/home/svn/banana/trunk@3 9869982d-c50d-0410-be91-f2a2ec7c7c7b

---
+++ examples/pj.php
@@ -0,0 +1,14 @@
+<?php
+/********************************************************************************
+* pj.php : for getting attachments
+* -----------
+*
+* This file is part of the banana distribution
+* Copyright: See COPYING files that comes with this distribution
+********************************************************************************/
+
+require_once("banana/banana.inc.php");
+$res = Banana::run();
+
+?>
+
