24f4446f254f96f904c150056424940db83bc7ea
x2003bruneau <x2003bruneau@9869982d-c50d-0410-be91-f2a2ec7c7c7b>
2006-04-08 12:46:10 +0000

    Fix les x-faces en php5...
    git-svn-id: svn+ssh://murphy/home/svn/banana/trunk@47 9869982d-c50d-0410-be91-f2a2ec7c7c7b

--- examples/xface.php
+++ examples/xface.php
@@ -1,6 +1,6 @@
 <?php
 
 header('Content-Type: image/jpeg');
-passthru('echo '.escapeshellarg(base64_decode($_REQUEST['face'])).'|uncompface -X |convert xbm:- jpg:-');
+passthru('echo '.escapeshellarg(base64_decode(str_replace(' ', '+', $_REQUEST['face']))).'|uncompface -X |convert xbm:- jpg:-');
 
 ?>
