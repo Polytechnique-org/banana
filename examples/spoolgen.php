b052aab700be8ff1e786df137be250bc4f51b4c5
x2003bruneau <x2003bruneau@9869982d-c50d-0410-be91-f2a2ec7c7c7b>
2007-01-15 10:32:52 +0000

    Merge a protocole independent version of spoolgen in Banana itself
    git-svn-id: svn+ssh://murphy/home/svn/banana/trunk@173 9869982d-c50d-0410-be91-f2a2ec7c7c7b

--- examples/spoolgen.php
+++ examples/spoolgen.php
@@ -20,46 +20,8 @@ EOF;
     exit;
 }
 
-class MyBanana extends Banana
-{
-    public function __construct()
-    {
-        global $opt;
-        Banana::$host = "news://{$opt['u']}:{$opt['p']}@localhost:119/\n";
-        echo Banana::$host;
-        parent::__construct();
-    }
-
-    private function checkErrors()
-    {
-        if (Banana::$protocole->lastErrno()) {
-            echo "\nL'erreur suivante s'est produite : "
-                . Banana::$protocole->lastErrno() . " "
-                . Banana::$protocole->lastError() . "\n";
-            exit;
-        }
-    }
-
-    public function createAllSpool()
-    {
-        $this->checkErrors();
-        $groups = Banana::$protocole->getBoxList();
-        $this->checkErrors();
-
-        foreach (array_keys($groups) as $g) {
-            print "Generating spool for $g : ";
-            Banana::$group = $g;
-            $spool = $this->loadSpool($g);
-            $this->checkErrors();
-            print "done.\n";
-            unset($spool);
-        }
-    }
-}
-
-
-$banana = new MyBanana();
-$banana->createAllSpool();
+Banana::$nntp_host = "news://{$opt['u']}:{$opt['p']}@localhost:119/\n";
+Banana::createAllSpool(array('NNTP'));
 
 // vim:set et sw=4 sts=4 ts=4
 ?>
