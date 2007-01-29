1e860b2a9d18be2ad5a3ced644585c1e1bf9bbb0
x2003bruneau <x2003bruneau@9869982d-c50d-0410-be91-f2a2ec7c7c7b>
2007-01-29 20:16:21 +0000

    Remove obsolete file
    git-svn-id: svn+ssh://murphy/home/svn/banana/trunk@185 9869982d-c50d-0410-be91-f2a2ec7c7c7b

--- examples/xface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('Content-Type: image/gif');
-passthru('echo ' . escapeshellarg(base64_decode($_REQUEST['face']))
-        . '| uncompface -X '
-        . '| convert -transparent white xbm:- gif:-');
-
-// vim:set et sw=4 sts=4 ts=4
-?>
